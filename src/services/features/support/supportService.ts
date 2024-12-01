e1208e884a7d806b3b1cf4e7d8d43dcc03ebf217
Gospel Jonathan <sirelite11@gmail.com>
2024-12-01 11:42:53 +0100

    cleaned the services

--- src/services/features/support/supportService.ts
+++ src/services/features/support/supportService.ts
@@ -2,14 +2,11 @@ import axiosClient from "@/services/api/axiosClient";
 // import axios from "axios";
 
 const send_message = async (data: IMessage) => {
-  console.log(data)
   const response = await axiosClient.post(
     "https://handyman-backend-4sb0.onrender.com/handyman/v1/support/send-message",
     data
   );
 
-  console.log(response.data);
-  
   return response.data;
 };
 
