43b7539d80a1d1304d740dcb44747c5c60c02296
mac <mac@OLUWADEMILADE-2.local>
2024-12-04 04:01:55 +0100

    just pushing

--- src/pages/HandymanAuth/RoleSelection/RoleSelection.tsx
+++ src/pages/HandymanAuth/RoleSelection/RoleSelection.tsx
@@ -4,29 +4,33 @@ import handymanImage from "@/assets/images/Frame 1597882487 (1).png";
 const RoleSelection = () => {
   return (
     <div className="flex flex-col items-center justify-center max-w-[791px] w-full mx-auto mt-[120px] mb-[150px]">
-      <div>
-        <h1 className="text-[40px] leading-[50.28px] font-bold text-[#191919]">
-          How would you like to use <span>Handyman?</span>
+      <div className="flex flex-col items-center gap-6 text-center">
+        <h1 className="text-[40px] leading-[50.28px] font-bold text-[#191919] px-24 font-merriweather">
+          How would you like to use{" "}
+          <span className="text-[#008080]">Handyman?</span>
         </h1>
-        <p>Select an option on how to use handyman</p>
+        <p className="text-[24px] leading-[32px] font-medium font-lato text-[#3C3C3C]">
+          Select an option on how to use handyman
+        </p>
       </div>
 
-      <div>
-        <div>
+      <form>
+        <div className="flex flex-row items-center justify-center">
           <div>
-            <img src={userImage} alt="user image" />
+            <div>
+              <img src={userImage} alt="user image" />
+            </div>
+            <p>Connect with skilled handymen for any home improvement need.</p>
           </div>
-          <p>Connect with skilled handymen for any home improvement need.</p>
-        </div>
-        <div>
           <div>
-            <img src={handymanImage} alt="user image" />
+            <div>
+              <img src={handymanImage} alt="user image" />
+            </div>
+            <p>Connect with skilled handymen for any home improvement need.</p>
           </div>
-          <p>Connect with skilled handymen for any home improvement need.</p>
         </div>
-
         <button>Continue</button>
-      </div>
+      </form>
     </div>
   );
 };
