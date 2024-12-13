2a3aa006c2c6f280b9e9bb5011dc3b82095b52c0
jerryemmanuel <jerryemmanuelolisa@gmail.com>
2024-12-13 23:52:24 +0100

    feat: profile form inteface completed

--- src/components/auth/CompleteProfileForm.tsx
+++ src/components/auth/CompleteProfileForm.tsx
@@ -1,10 +1,14 @@
+import useCompleteProfileForm from "@/hooks/formhooks/useCompleteProfileForm";
+
 const CompleteProfileForm = () => {
+  const { formik, isLoading, getLGAs, statefile } = useCompleteProfileForm();
+
   return (
-    <form className="mt-6 lg:mb-20">
+    <form className="mt-6 lg:mb-20" onSubmit={formik.handleSubmit}>
       <div className="lg:flex items-center gap-6">
         <div className="w-full">
           <label
-            htmlFor="street"
+            htmlFor="address"
             className="font-medium text-sm tracking-wide lg:text-base"
           >
             Street Address
@@ -13,12 +17,20 @@ const CompleteProfileForm = () => {
           <div className="h-10 lg:h-14 px-3 lg:px-4 border border-[#D0D5DD] rounded-md">
             <input
               type="text"
-              name="street"
-              id="street"
+              name="address"
+              id="address"
               placeholder="Enter your street address"
               className="w-full h-full outline-none text-xs lg:text-sm"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.address}
             />
           </div>
+          {formik.touched.address && formik.errors.address ? (
+            <div className="text-red-500 text-[10px] lg:text-xs font-semibold">
+              {formik.errors.address}
+            </div>
+          ) : null}
         </div>
         <div className="mt-6 lg:mt-0 w-full">
           <label
@@ -28,14 +40,28 @@ const CompleteProfileForm = () => {
             Select State
           </label>
           <br />
-          <div className="h-10 lg:h-14 px-3 lg:px-4 border border-[#D0D5DD] rounded-md">
-            <input
-              type="text"
+          <div className="h-10 lg:h-14 border p border-[#D0D5DD] rounded-md">
+            <select
               name="state"
               id="state"
-              placeholder="Enter your street address"
-              className="w-full h-full outline-none text-xs lg:text-sm"
-            />
+              className="w-full h-full px-3 lg:px-4 text-xs lg:text-sm outline-none"
+              aria-placeholder="Please select state"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.state}
+            >
+              <option value=""></option>
+              {statefile.map((val, i) => (
+                <option key={i} value={val.state}>
+                  {val.state}
+                </option>
+              ))}
+            </select>
+            {formik.touched.state && formik.errors.state ? (
+              <div className="text-red-500 text-[10px] lg:text-xs font-semibold">
+                {formik.errors.state}
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
@@ -45,13 +71,28 @@ const CompleteProfileForm = () => {
             Select LGA
           </label>
           <br />
-          <div className="h-10 lg:h-14 px-3 lg:px-4 border border-[#D0D5DD] rounded-md">
-            <input
-              type="text"
-              name="LGA"
-              id="LGA"
-              className="w-full h-full outline-none text-xs"
-            />
+          <div className="h-10 lg:h-14 border border-[#D0D5DD] rounded-md">
+            <select
+              disabled={!formik.values.state}
+              name="lga"
+              id="lga"
+              className="w-full h-full px-3 lg:px-4 text-xs lg:text-sm outline-none"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.lga}
+            >
+              <option value=""></option>
+              {getLGAs(formik.values.state).map((val, i) => (
+                <option key={i} value={val}>
+                  {val}
+                </option>
+              ))}
+            </select>
+            {formik.touched.lga && formik.errors.lga ? (
+              <div className="text-red-500 text-[10px] lg:text-xs font-semibold">
+                {formik.errors.lga}
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
@@ -71,8 +112,16 @@ const CompleteProfileForm = () => {
               id="firstName"
               placeholder="Enter your first name"
               className="w-full h-full outline-none text-xs lg:text-sm"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.firstname}
             />
           </div>
+          {formik.touched.firstname && formik.errors.firstname ? (
+            <div className="text-red-500 text-[10px] lg:text-xs font-semibold">
+              {formik.errors.firstname}
+            </div>
+          ) : null}
         </div>
         <div className="mt-6 lg:mt-0 w-full">
           <label
@@ -89,8 +138,16 @@ const CompleteProfileForm = () => {
               id="lastName"
               placeholder="Enter your last name"
               className="w-full h-full outline-none text-xs lg:text-sm"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.lastname}
             />
           </div>
+          {formik.touched.lastname && formik.errors.lastname ? (
+            <div className="text-red-500 text-[10px] lg:text-xs font-semibold">
+              {formik.errors.lastname}
+            </div>
+          ) : null}
         </div>
       </div>
       <div className="mt-6 lg:flex items-center gap-6">
@@ -109,34 +166,54 @@ const CompleteProfileForm = () => {
               id="phone"
               placeholder="Enter your phone number"
               className="w-full h-full outline-none text-xs lg:text-sm"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.phone}
             />
           </div>
+          {formik.touched.phone && formik.errors.phone ? (
+            <div className="text-red-500 text-[10px] lg:text-xs font-semibold">
+              {formik.errors.phone}
+            </div>
+          ) : null}
         </div>
         <div className="mt-6 lg:mt-0 w-full">
           <label
-            htmlFor="state"
+            htmlFor="gender"
             className="font-medium text-sm lg:text-base tracking-wide"
           >
             Gender
           </label>
           <br />
-          <div className="h-10 lg:h-14 px-3 lg:px-4 border border-[#D0D5DD] rounded-md">
-            <input
-              type="text"
+          <div className="h-10 lg:h-14 border border-[#D0D5DD] rounded-md">
+            <select
               name="gender"
               id="gender"
-              placeholder=""
-              className="w-full h-full outline-none text-xs lg:text-sm"
-            />
+              className="w-full h-full px-3 lg:px-4 text-xs lg:text-sm outline-none"
+              aria-placeholder="Please select state"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.gender}
+            >
+              <option value=""></option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
           </div>
+          {formik.touched.gender && formik.errors.gender ? (
+            <div className="text-red-500 text-[10px] lg:text-xs font-semibold">
+              {formik.errors.gender}
+            </div>
+          ) : null}
         </div>
       </div>
       <div className="mt-6 lg:mt-12 w-full flex items-center justify-center">
         <button
           type="submit"
-          className="bg-[#008080] h-8 lg:h-14 rounded-lg w-full lg:w-[385px] text-white font-semibold text-xs lg:text-lg"
+          disabled={!formik.dirty || !formik.isValid || isLoading}
+          className={`${!formik.dirty || !formik.isValid || isLoading ? "bg-[#D0D5DD]" : "bg-[#008080]"} w-full lg:w-[385px]  text-white rounded-lg h-10 lg:h-14 px-6 mt-8 lg:mt-16 text-xs lg:text-base lg:font-semibold outline-none`}
         >
-          Done
+          {isLoading ? "Please wait..." : "Done"}
         </button>
       </div>
     </form>
--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -62,7 +62,7 @@ export const loginSchema = Yup.object().shape({
     .matches(
       /[\W_]/,
       "Password must contain at least one special character (e.g., $, &, @, etc.)"
-    )
+    ),
 });
 
 export const forgetPasswordSchema = Yup.object().shape({
@@ -84,4 +84,23 @@ export const resetPasswordSchema = Yup.object().shape({
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("password")], "Passwords must match")
     .required("Please confirm your password"),
+});
+
+export const completeProfileSchema = Yup.object().shape({
+  address: Yup.string()
+    .min(5, "Address must be at least 5 character long")
+    .required("Address is required"),
+  state: Yup.string().required("State is required"),
+  lga: Yup.string().required("State is required"),
+  firstname: Yup.string()
+    .required("State is required")
+    .min(2, "Name must be at least 2 characters"),
+  lastname: Yup.string()
+    .required("State is required")
+    .min(2, "Name must be at least 2 characters"),
+  phone: Yup.string()
+    .min(10, "Phone number must be at least 11 characters")
+    .max(15, "Phone number should be less than 15 characters long.")
+    .matches(/^\d+$/, "Only numbers are allowed"),
+  gender: Yup.string().required("State is required"),
 });
